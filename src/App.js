07d882807186c3353fb8c797d2f8250eb9a3e9b4
Mariusz Kurek <mariusz53710@gmail.com>
2021-04-30 13:44:54 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch, Redirect } from "react-router-dom";
 import Posts from "./components/posts";
+import Home from "./components/home";
+import NavBar from "./components/common/navbar";
 
 function App() {
-  return (<Posts/>
+  return (
+      <div className="container-fluid">
+        <NavBar />
+        <div className="container">
+          <div className="content">
+            <Switch>
+              <Route path="/posts"
+                     component={Posts} />}
+              />
+              <Route path="/" exact component={Home} />
+              <Redirect to="/not-found" />
+            </Switch>
+          </div>
+        </div>
+      </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,10 +8,12 @@ import 'font-awesome/css/font-awesome.min.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </BrowserRouter>,
+document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
